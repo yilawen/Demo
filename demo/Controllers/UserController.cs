e66f572611beedc5722af9c94c000aee8c5d40ef
yilawen <841350396@qq.com>
2016-12-12 13:50:59 +0800

    add user C and M

---
+++ demo/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demo.Models;
+
+namespace demo.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public JsonResult Login(string username, string password)
+        {
+            UserModel user = new UserModel();
+            bool result = user.authenticate(username, password);
+            return Json(result, JsonRequestBehavior.DenyGet);
+        }
+
+    }
+}
---
+++ demo/Models/UserModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace demo.Models
+{
+    public class UserModel
+    {
+        private SqlConnection conn;
+        private SqlCommand com = new SqlCommand();
+        private SqlDataReader sdr = null;
+
+        public UserModel()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["UserDBContext"].ConnectionString;
+            conn = new SqlConnection(connectionString);
+            try
+            {
+                if (conn.State.ToString() != "Open")
+                {
+                    conn.Open();
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool authenticate(string username, string password)
+        {
+            string sql = "select password from [user] where username = @username";
+            com.CommandText = sql;
+            com.CommandType = CommandType.Text;
+            com.Connection = conn;
+            com.Parameters.Add("@username", SqlDbType.VarChar);
+            com.Parameters["@username"].Value = username;
+            sdr = com.ExecuteReader();
+            if (sdr.Read())
+            {
+                if (sdr["password"].Equals(password)) return true;
+                else return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

8e7a5c580f1b146a798682ea44c890f76f65d110
Lli123 <wdwyyx1995@163.com>
2016-12-14 14:42:41 +0800

    22

--- demo/Scripts/login.js
+++ demo/Scripts/login.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	// $("#btn").click(function(){
 	// 	$(selector).post(URL,data,function(data,status,xhr),dataType)
 	// 	$.post(getRootPath() + "/user/login",{
@@ -7,23 +7,29 @@ $(document).ready(function(){
 	// 	})
 	// },
 
-	// )
-	console.log(88)
+	// )	console.log(88)
+	// $("body").on("focus",".form-control",function(){
+	// 	$(this).closest(".fg-line").addClass(".fp")
+	// })
+
 
 	$.ajax({
-    method: 'post',
-    url: getRootPath() + "/user/login",
-    dataType: 'json',
-    // 用户名和密码
-    data: {username: $("#uname").val(), password: $("#upwd").val()},
-    success: function(data){
-    	console.log(data);
-        // 假设后端传回的 1 表示成功， 0 表示用户名或密码错误
-        if (data) {
-            alert("验证通过");
-        } else {
-            alert("用户名或密码错误");
-        }
-    }
-});
+		method: 'post',
+		url: getRootPath() + "/user/login",
+		dataType: 'json',
+		// 用户名和密码
+		data: {
+			username: $("#uname").val(),
+			password: $("#upwd").val()
+		},
+		success: function(data) {
+			console.log(data);
+			// 假设后端传回的 1 表示成功， 0 表示用户名或密码错误
+			if (data) {
+				alert("验证通过");
+			} else {
+				alert("用户名或密码错误");
+			}
+		}
+	});
 })

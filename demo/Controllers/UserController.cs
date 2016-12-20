dc7f2030b7bbadc7d9f1b31efc38961549a8e105
yilawen <841350396@qq.com>
2016-12-20 13:49:53 +0800

    完成登录功能

---
+++ demo/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demo.Models;
+
+namespace demo.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            if (Session["user"] != null) {
+                ViewBag.user = Session["user"];
+                return View("index");
+            } else return View("Login");
+        }
+
+        public JsonResult Login(string username, string password)
+        {
+            using (UserModel userModel = new UserModel())
+            {
+                bool result = userModel.Authenticate(username, password);
+                if (result) {
+                    var user = userModel.GetUserByUsername(username);
+                    user.LastLoginDate = System.DateTime.Now;
+                    user.LastLoginIP = System.Web.HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+                    Session["user"] = user;
+                    userModel.UpdateUser(user, new string[]{"LastLoginDate", "LastLoginIP"});
+                }
+                return Json(result, JsonRequestBehavior.DenyGet);
+            }
+        }
+
+        public RedirectToRouteResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}

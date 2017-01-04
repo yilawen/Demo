0cf3ca29d81d10b3775390c1b5c2e67a690b0d9f
Lli123 <wdwyyx1995@163.com>
2017-01-04 14:03:41 +0800

    2

--- demo/Scripts/layout.js
+++ demo/Scripts/layout.js
@@ -1,42 +1,42 @@
 ﻿$(function() {
-    var root = getRootPath();
-    var currentPasswordBox = $("#current_password");
-    var currentAckPasswordBox = $("#current_ack_password");
-    $("#current_user_save").on("click", function() {
-        if(!currentPasswordBox.val() || !currentAckPasswordBox.val()) {
-            alert("密码不能为空！");
-            return;
+  var root = getRootPath();
+  var currentPasswordBox = $("#current_password");
+  var currentAckPasswordBox = $("#current_ack_password");
+  $("#current_user_save").on("click", function() {
+    if (!currentPasswordBox.val() || !currentAckPasswordBox.val()) {
+      alert("密码不能为空！");
+      return;
+    }
+    if (currentPasswordBox.val() != currentAckPasswordBox.val()) {
+      alert("密码不一致！");
+    } else {
+      $.ajax({
+        url: root + "/user/UpdateCurrentUserPassword",
+        type: "post",
+        dataType: "json",
+        data: {
+          password: currentPasswordBox.val()
+        },
+        success: function(result) {
+          if (result.status) {
+            $("#current_user_modal").modal("hide");
+            saveSuccessModal();
+            setTimeout(function() {
+              window.location.href = root + "/user/logout";
+            }, 1000)
+          } else {
+            alert(result.message);
+          }
         }
-        if(currentPasswordBox.val() != currentAckPasswordBox.val()) {
-            alert("密码不一致！");
-        } else {
-            $.ajax({
-                url: root + "/user/UpdateCurrentUserPassword",
-                type: "post",
-                dataType: "json",
-                data: {
-                    password: currentPasswordBox.val()
-                },
-                success: function(result) {
-                    if(result.status) {
-                        $("#current_user_modal").modal("hide");
-                         saveSuccessModal();
-                        setTimeout(function(){
-                            window.location.href = root + "/user/logout";
-                        },1000)
-                    } else {
-                        alert(result.message);
-                    }
-                }
-            });
-        }
-    });
+      });
+    }
+  });
 
-    $("#btn_current_pwd").on("click", function() {
-        currentPasswordBox.val("");
-        currentAckPasswordBox.val("");
-    });
-    $("body").on("keydown", "input", function(event) {
-                if(event.keyCode == 13) $("#current_user_save").click();
-         });
+  $("#btn_current_pwd").on("click", function() {
+    currentPasswordBox.val("");
+    currentAckPasswordBox.val("");
+  });
+  $("#current_user_modal").on("keydown", "input", function(event) {
+    if (event.keyCode == 13) $("#current_user_save").click();
+  });
 })
